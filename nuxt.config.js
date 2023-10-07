d2262382910889fb6ccb659627f40e23181c7974
mwieja <m.wieja@qarbon.it>
2023-10-07 14:07:38 +0200

    font size fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  server: {
+    host: '0.0.0.0',
+    port: 8000
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
